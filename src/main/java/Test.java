3abc70acb5e00895c281a87b1dff1fba146b0a26
Mark <1405144502@qq.com>
2023-10-22 15:11:12 +0800

    first commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("Test===========================");
         Scanner scanner=new Scanner(System.in);
 
         while (scanner.hasNext()){
